336655c6b54fc68c2b2e108c8db1d6818322bc48
MaxMakau <127086966+MaxMakau@users.noreply.github.com>
2025-05-30 22:19:47 +0300

    Update settings.py db to postgres

--- elevateAcademy/settings.py
+++ elevateAcademy/settings.py
@@ -71,7 +71,7 @@ WSGI_APPLICATION = "elevateAcademy.wsgi.application"
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:Kidd9637@localhost:5432/innovateacademy',
+        default='postgresql://postgres:postgres@localhost:5432/innovateacademy',
         conn_max_age=600
     )
 }
