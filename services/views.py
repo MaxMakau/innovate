3a0084d8a92f59ad2cc9ac7c28267427784b201c
MaxMakau <127086966+MaxMakau@users.noreply.github.com>
2025-05-30 22:22:27 +0300

    Update views.py removed unnessary filters

--- services/views.py
+++ services/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from services.models import BlogSection, UpcomingEvent,Achievement,Startup
+from services.models import BlogSection, UpcomingEvent,Achievement
 from startups.models import Startup  # Updated import
 
 def homepage(request):
-    startups = list(Startup.objects.all().order_by('-registrationDate')[:3])
+    startups = list(Startup.objects.all()
     achievements = Achievement.objects.all()
-    latest_blog = list(BlogSection.objects.all().order_by('-created_date')[:3])
-    upcoming_events = list(UpcomingEvent.objects.all().order_by('-created_date')[:3])  # Get 3 upcoming events
+    latest_blog = list(BlogSection.objects.all()
+    upcoming_events = list(UpcomingEvent.objects.all()
     context = {
         'latest_blog': latest_blog,
         'upcoming_events': upcoming_events,
