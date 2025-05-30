61390205abff8a2f7dce2cac7af42a37f3761d4f
Mtulivu9637 <mtulivukidd@gmail.com>
2025-05-30 21:59:20 +0300

    updates to homepage, courses, services, startups, users

--- services/views.py
+++ services/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from services.models import BlogSection, UpcomingEvent,Achievement
-from startups.models import Startup
+from services.models import BlogSection, UpcomingEvent,Achievement,Startup
+from startups.models import Startup  # Updated import
 
 def homepage(request):
-    startups = Startup.objects.all()
+    startups = list(Startup.objects.all().order_by('-registrationDate')[:3])
     achievements = Achievement.objects.all()
-    latest_blog = BlogSection.objects.all()
-    upcoming_events = UpcomingEvent.objects.all()  # Get 3 upcoming events
+    latest_blog = list(BlogSection.objects.all().order_by('-created_date')[:3])
+    upcoming_events = list(UpcomingEvent.objects.all().order_by('-created_date')[:3])  # Get 3 upcoming events
     context = {
         'latest_blog': latest_blog,
         'upcoming_events': upcoming_events,
