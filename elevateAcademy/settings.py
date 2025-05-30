341e25dbd066f9d5dc3c79c5d305c2a073f6085c
oduorbuta <161636969+oduorbuta@users.noreply.github.com>
2025-05-30 11:06:04 +0300

    Update settings.py set postgrel database

--- elevateAcademy/settings.py
+++ elevateAcademy/settings.py
@@ -81,11 +81,13 @@ WSGI_APPLICATION = "elevateAcademy.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/innovateacademy',
+        conn_max_age=600
+    )
 }
+
 
 
 # Password validation
